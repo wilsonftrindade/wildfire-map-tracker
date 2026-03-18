d219b5bedb94d39c17bb598011ee6dbb7272bf01
Wilson Trindade <wilsonftneto@gmail.com>
2026-03-17 20:21:03 -0500

    Fetch Nasa API data and create loader component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import Map from "./components/Map"
+import { useEffect, useState } from "react";
+import Map from './components/Map'
+import Loader from './components/Loader'
 
 function App() {
+  const [eventData, setEventData] = useState ([])
+  const[loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      setLoading(true)
+      const res = await fetch('https://eonet.gsfc.nasa.gov/api/v2.1/events')
+      const {events} = await res.json()
+
+      setEventData(events)
+      setLoading(false)
+    }
+
+    fetchEvents()
+  }, [])
+
   return (
     <div>
-      <Map/>
+      {!loading ? <Map/> : <Loader/>}
     </div>
   )
 };
---
+++ src/components/Loader.js
@@ -0,0 +1,12 @@
+import loading from './loading.gif'
+
+const Loader = () => {
+  return (
+    <div className='loader'>   
+        <img src = {loading} alt = "Loading" />
+        <h1>Fetching Data</h1>
+    </div>
+  )
+}
+
+export default Loader;
